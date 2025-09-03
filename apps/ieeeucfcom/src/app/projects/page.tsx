4aaa224342fe1a3e1432e4694dee200113b34800
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2025-09-03 10:39:32 -0400

    icon link missing

--- apps/ieeeucfcom/src/app/projects/page.tsx
+++ apps/ieeeucfcom/src/app/projects/page.tsx
@@ -6,7 +6,7 @@ import ProjectsPage from "@/components/pg/projectspage"
 import { Metadata } from "next";
 
 const pageTitle = "Projects | IEEE UCF";
-const pageDescription = "Explore IEEE @ UCF projects to tackle real-world challenges, build technical skills, and collaborate with peers.";
+const pageDescription = "Explore IEEE UCF projects to tackle real-world challenges, build technical skills, and collaborate with peers.";
 
 export const metadata: Metadata = {
     title: pageTitle,
