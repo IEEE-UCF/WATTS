fd4a94cd986fce41d7726862d146d4d2115a6cc8
MsMarion <dawnbalaschak@gmail.com>
2025-11-14 02:35:45 -0500

    Hopefully 2nd to last

--- apps/ieeeucfcom/src/app/api/events/route.ts
+++ apps/ieeeucfcom/src/app/api/events/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from 'next/server';
 // import { DateTime } from "luxon";
 // const uri = process.env.MONGODB_URI!;
 // const client = new MongoClient(uri);
-// const dbName = "IEEE-Website"; 
+// const dbName = "IEEE-Website";
 // export async function GET() {
 //   await dbConnect();
 //   await client.connect();
@@ -21,17 +21,16 @@ import { NextResponse } from 'next/server';
 //         utcTime = DateTime.fromJSDate(event.time, { zone: 'utc' });
 //       }
 //       const easternTime = utcTime.setZone('America/New_York').toFormat('MMMM d, yyyy h:mm a');
-
 //       return {
 //         ...event,
-//         time: easternTime, 
+//         time: easternTime,
 //       };
 //     });
 //     return NextResponse.json({ success: true, data: convertedEvents });
 //   } catch (error) {
 //     console.error(error);
 //     return NextResponse.json({ success: false, error: 'Failed to fetch events' }, { status: 500 });
-//   
+//
 // }
 
 export async function GET() {
