9cd4116a18b0aebe83f7b17237f8d8084ef5ab74
MsMarion <dawnbalaschak@gmail.com>
2025-11-18 10:10:43 -0500

    Commented out.

--- apps/ieeeucfcom/src/components/pg/memberqrcodescan.tsx
+++ apps/ieeeucfcom/src/components/pg/memberqrcodescan.tsx
@@ -1,25 +1,25 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import QrReader from "qrcode";
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import QrReader from "qrcode";
 
-const Test = (props) => {
-  const [qrResult, setQrResult] = useState<string>("no result");
+// const Test = (props) => {
+//   const [qrResult, setQrResult] = useState<string>("no result");
 
-  return (
-    <>
-      <QrReader
-        onScan={(data) => {
-          if (data) {
-            setQrResult(data);
-          }
-        }}
-        onError={(err) => {
-          console.error(err);
-        }}
-      />
-      <p>QR Code Result: {qrResult}</p>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <QrReader
+//         onScan={(data) => {
+//           if (data) {
+//             setQrResult(data);
+//           }
+//         }}
+//         onError={(err) => {
+//           console.error(err);
+//         }}
+//       />
+//       <p>QR Code Result: {qrResult}</p>
+//     </>
+//   );
+// };
 
-export default Test;
+// export default Test;
