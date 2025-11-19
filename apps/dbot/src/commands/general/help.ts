dd30e476ca390c6df2bac98abb241a6f47d3e3b0
Thandi Menelas <TMenelas@outlook.com>
2025-11-19 04:30:10 -0500

    Fixed help command not loading

--- apps/dbot/src/commands/general/help.ts
+++ apps/dbot/src/commands/general/help.ts
@@ -5,7 +5,7 @@ import { PermissionLevel } from '../../modules/helpers/Utils.js';
 export class HelpCommand extends Command {
 	constructor(client: any) {
 		super(client, {
-			name: 'Help',
+			name: 'help',
 			description: 'Displays a list useful commands and how to use them',
 			usage: 'help',
 			category: 'general',
@@ -15,24 +15,24 @@ export class HelpCommand extends Command {
 		});
 	}
 
-    async run(interaction: ChatInputCommandInteraction): Promise<void> {
-            const embed = this.client.createEmbed()
-                .setThumbnail(this.client.user?.displayAvatarURL() ?? null)
-                .setTitle(`${this.client.user?.username ?? 'Larry'} Help`)
-                .setDescription([
-                    `hello twin...`,
-                ].join('\n'))
-                .setFooter({
-                    text: `Requested by ${interaction.user.username} • ${this.client.config.embed.footer}`,
-                    iconURL: interaction.user.displayAvatarURL({ size: 1024 }),
-                });
+	async run(interaction: ChatInputCommandInteraction): Promise<void> {
+		const embed = this.client.createEmbed()
+			.setThumbnail(this.client.user?.displayAvatarURL() ?? null)
+			.setTitle(`${this.client.user?.username ?? 'Larry'} Help`)
+			.setDescription([
+				'hello twin...',
+			].join('\n'))
+			.setFooter({
+				text: `Requested by ${interaction.user.username} • ${this.client.config.embed.footer}`,
+				iconURL: interaction.user.displayAvatarURL({ size: 1024 }),
+			});
 
-            await interaction.reply({
-                embeds: [embed],
-            });
+		await interaction.reply({
+			embeds: [embed],
+		});
 	}
 
-    command(): SlashCommandBuilder {
+	command(): SlashCommandBuilder {
 		return new SlashCommandBuilder()
 			.setName(this.name)
 			.setDescription(this.description);
