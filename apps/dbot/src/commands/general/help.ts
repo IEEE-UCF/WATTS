e73d8c4efd41018de571e7fb54bac390a4a68ff5
Thandi Menelas <TMenelas@outlook.com>
2025-11-20 01:02:28 -0500

    Fixed help command modules

--- apps/dbot/src/commands/general/help.ts
+++ apps/dbot/src/commands/general/help.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
-import { Command } from '../../structs/Command.js';
-import { PermissionLevel } from '../../modules/helpers/Utils.js';
+import { Command } from '../../structs/Command.ts';
+import { PermissionLevel } from '../../modules/helpers/Utils.ts';
 
 export class HelpCommand extends Command {
 	constructor(client: any) {
