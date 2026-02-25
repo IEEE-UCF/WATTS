7241a5d81b91f7c4d04195b49a1df9dd69e7a15f
Kian Bayzid <134166704+kianbayzid@users.noreply.github.com>
2026-02-25 16:09:12 -0500

    Fixed Greetings and config
    Added formatting changes to greetings and fixed constructor. Added general into both config files

--- apps/dbot/src/events/Greetings.ts
+++ apps/dbot/src/events/Greetings.ts
@@ -4,7 +4,7 @@ import {Event} from "../structs/Event.ts";
 export class Greetings extends Event {
     constructor(client: any) {
         super(client, {
-            name: 'Greetings',
+            name: 'guildMemberAdd',
             once: false,
         });
     }
@@ -16,8 +16,8 @@ export class Greetings extends Event {
         
         if (generalChannel?.isTextBased()) {
             const serverEmbed = this.client.createEmbed()
-                .setTitle('Welcome to IEEE Discord Server!')
-                .setDescription(`Hello ${member.user}, welcome to the IEEE UCF Discord! Please make sure to read the rules and enjoy your stay!`)
+                .setTitle('Welcome to the IEEE @ UCF Discord Server!')
+                .setDescription(`Hello ${member.user}, Welcome to the IEEE @ UCF Discord Server! Please make sure to read the rules, check out the server guide, and enjoy your stay!`)
                 .setThumbnail(member.user.displayAvatarURL({ size: 256 }))
             
             await generalChannel.send({ embeds: [serverEmbed] }).catch(console.error);
@@ -27,8 +27,8 @@ export class Greetings extends Event {
             const dmEmbed = new EmbedBuilder()
                 .setColor(this.client.config.embed.color)
                 .setTitle('Welcome to IEEE UCF!')
-                .setDescription(`Thanks for joining the **IEEE @ UCF** Discord server. Here are a few things to get started:\n` +
-                                `IEEE UCF is the University of Central Florida’s student chapter of the Institute of Electrical and Electronics Engineers, focused on helping students grow in engineering, computer science, professionalism, and service. The organization hosts workshops, meetings, career events, and long-term projects that give members hands-on experience and opportunities to present research https://ieee.cecs.ucf.edu/\n\n`
+                .setDescription(`Thanks for joining the **IEEE @ UCF** Discord server.\n` +
+                                `IEEE UCF is the University of Central Florida’s student chapter of the Institute of Electrical and Electronics Engineers, focused on helping students grow in engineering, computer science, professionalism, and service. The organization hosts workshops, meetings, career events, and long-term projects that give members hands-on experience and opportunities to present research for more information, visit:  https://ieee.cecs.ucf.edu/\n\n`
                 )
                 .setFooter({ text: this.client.config.embed.footer})
                 .setTimestamp();
