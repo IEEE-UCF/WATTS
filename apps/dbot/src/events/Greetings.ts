becfb8fa3665063e3bae4277baa1e232a1c84b4f
anthonysokolov76 <an711886@ucf.edu>
2026-03-04 19:16:56 -0500

    Updated greetings format, dependencies

--- apps/dbot/src/events/Greetings.ts
+++ apps/dbot/src/events/Greetings.ts
@@ -17,7 +17,7 @@ export class Greetings extends Event {
         if (generalChannel?.isTextBased()) {
             const serverEmbed = this.client.createEmbed()
                 .setTitle('Welcome to the IEEE @ UCF Discord Server!')
-                .setDescription(`Hello ${member.user}, Welcome to the IEEE @ UCF Discord Server! Please make sure to read the rules, check out the server guide, and enjoy your stay! Use the /help command to get started`)
+                .setDescription(`Hello ${member.user}, Welcome to the IEEE @ UCF Discord Server! Please make sure to read the rules, check out the server guide, and enjoy your stay! Use the /help command to get started.`)
                 .setThumbnail(member.user.displayAvatarURL({ size: 256 }))
             
             await generalChannel.send({ embeds: [serverEmbed] }).catch(console.error);
@@ -28,7 +28,7 @@ export class Greetings extends Event {
                 .setColor(this.client.config.embed.color)
                 .setTitle('Welcome to IEEE UCF!')
                 .setDescription(`Thanks for joining the **IEEE @ UCF** Discord server.\n` +
-                                `IEEE UCF is the University of Central Florida’s student chapter of the Institute of Electrical and Electronics Engineers, focused on helping students grow in engineering, computer science, professionalism, and service. The organization hosts workshops, meetings, career events, and long-term projects that give members hands-on experience and opportunities to present research for more information, visit:  https://ieee.cecs.ucf.edu/\n\n`
+                                `IEEE UCF is the University of Central Florida’s student chapter of the Institute of Electrical and Electronics Engineers, focused on helping students grow in engineering, computer science, professionalism, and service. The organization hosts workshops, meetings, career events, and long-term projects that give members hands-on experience and unique opportunities. For more information, visit:  https://ieee.cecs.ucf.edu/\n\n`
                 )
                 .setFooter({ text: this.client.config.embed.footer})
                 .setTimestamp();
