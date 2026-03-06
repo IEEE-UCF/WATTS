60a78629c7eebe290d80c1b425fd2c701ac8d39f
Jeremy Achong <jeremyachong@gmail.com>
2026-03-06 00:13:10 -0500

    playing with events border

--- apps/ieeeucfcom/src/components/dashboard/event-list.tsx
+++ apps/ieeeucfcom/src/components/dashboard/event-list.tsx
@@ -26,24 +26,22 @@ export const EventList = () => {
 
   return (
     <Card className="w-full max-w-4xl mx-auto mt-8 rounded-xl bg-black shadow-sm shadow-[0_0_20px_rgba(250,204,21,0.5)] text-card-foreground flex flex-col p-6 ">
-      <CardTitle className="text-2xl font-[subheading-font] mb-6 text-white">
+      <CardTitle className="text-2xl font-[subheading-font] -mb-2 text-white">
         Events
       </CardTitle>
       <CardContent>
         <div className="w-full max-w-4xl mx-auto">
-          <ScrollArea className={`${events.length > 3 ? "h-80" : ""}`}>
-            {/* <Separator className="divide-y divide-gray-200"> */}
+          <ScrollArea
+            className={`${events.length > 3 ? "h-80" : ""} rounded-md border-[var(--ieee-grey)] `}
+          >
             {events.length === 0 ? (
               <p className="p-4 text-center text-[var(--ieee-white)]">
                 No events found.
               </p>
             ) : (
-              events.map((event) => (
-                <Card
-                  key={event.id}
-                  className="mb-2 bg-[var(--ieee-dark-grey)] rounded-lg shadow-md p-2"
-                >
-                  <div key={event.id} className="p-4">
+              events.map((event, index) => (
+                <Card className="rounded-md border-[var(--ieee-grey)] mb-4">
+                  <div className="p-4">
                     <h3 className="text-xl font-semibold text-white">
                       {event.title}
                     </h3>
@@ -52,12 +50,11 @@ export const EventList = () => {
                     </p>
                     <p className="text-gray-600">
                       {new Date(event.startTime).toLocaleString(undefined, {
-						dateStyle: "medium",
-						timeStyle: "short",
-					  })}
+                        dateStyle: "medium",
+                        timeStyle: "short",
+                      })}
                     </p>
                   </div>
-                  <Separator />
                 </Card>
               ))
             )}
